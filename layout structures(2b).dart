02e1a2ed3821f58cf249e9928c474b481c75ca69
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2025-08-29 12:46:55 +0530

    Create layout structures(2b).dart

---
+++ layout structures(2b).dart
@@ -0,0 +1,85 @@
+A.Row layout:
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Row Layout')),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment
+              .spaceEvenly, // Spaces children evenly in the row
+          children: <Widget>[
+            Container(color: Colors.red, width: 100, height: 100),
+            Container(color: Colors.green, width: 100, height: 100),
+            Container(color: Colors.blue, width: 100, height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+B.column layout:
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Column Layout')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment
+              .spaceEvenly, // Spaces children evenly in the column
+          children: <Widget>[
+            Container(color: Colors.red, width: 100, height: 100),
+            Container(color: Colors.green, width: 100, height: 100),
+            Container(color: Colors.blue, width: 100, height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+C.stack widget:
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // ✅ Added Key
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Stack Layout')),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Container(color: Colors.red, width: 200, height: 200),
+              Container(color: Colors.green, width: 150, height: 150),
+              Container(color: Colors.blue, width: 100, height: 100),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
