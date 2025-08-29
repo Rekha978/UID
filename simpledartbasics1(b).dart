ad9e98cc3b4b3441c13de77f59c552a8d2f375c8
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2025-08-29 12:40:38 +0530

    Create simpledartbasics1(b).dart

---
+++ simpledartbasics1(b).dart
@@ -0,0 +1,22 @@
+void main() {
+  String name = 'Uday';
+  int age = 31;
+  double height = 5.8;
+  bool isTeacher = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Teacher: $isTeacher');
+  List<String> subjects = ['Dart', 'Flutter', 'AI', 'Python'];
+  print('\nSubjects Known:');
+  for (String subject in subjects) {
+    print('- $subject');
+  }
+
+  greetUser(name);
+}
+
+void greetUser(String userName) {
+  print('\nWelcome, $userName! Enjoy learning Dart.');
+}
