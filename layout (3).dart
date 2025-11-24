72154f14862305e106108a0c4d50eaa27a85d4fc
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2025-11-25 05:15:46 +0530

    Update and rename layout structures(2b).dart to layout (3).dart

--- layout (3).dart
+++ layout (3).dart
@@ -1,3 +1,64 @@
+
+import 'package:flutter/material.dart'; 
+void main() => runApp(const MaterialApp(home: LayoutDemo())); 
+class LayoutDemo extends StatelessWidget { 
+const LayoutDemo({super.key}); 
+@override 
+Widget build(BuildContext context) { 
+return Scaffold( 
+appBar: AppBar(title: const Text('Layouts Demo')), body: Column( 
+mainAxisAlignment: MainAxisAlignment.center, 
+children: [ 
+Row( 
+mainAxisAlignment: MainAxisAlignment.center,
+children: const [ 
+Icon(Icons.star, color: Colors.red), 
+Icon(Icons.star, color: Colors.green), 
+Icon(Icons.star, color: Colors.blue), 
+], 
+), 
+const SizedBox(height: 20), 
+Stack( 
+alignment: Alignment.center, 
+children: [ 
+Container(width: 100, height: 100, color: 
+Colors.blue), 
+Container(width: 60, height: 60, color: 
+Colors.yellow), 
+], 
+), 
+], 
+), 
+); 
+} 
+} 
+
+
+
+import 'package:flutter/material.dart'; 
+void main() => runApp(const MaterialApp(home: MyWidget())); 
+class MyWidget extends StatelessWidget { 
+const MyWidget({super.key}); 
+@override 
+Widget build(BuildContext context) { 
+return const Scaffold( 
+body: Center(child: Text('This is a Stateless Widget')), ); 
+} 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 A.Row layout:
 import 'package:flutter/material.dart';
 void main() {
